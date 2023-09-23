0693a7e553d163573597f05103c45d69d732302c
taenam <tkim356@wisc.edu>
2023-09-23 16:32:37 -0500

    fixed conflict

--- main.py
+++ main.py
@@ -28,36 +28,47 @@ def generate_rent_plot(rent_data, university_name, axes):
     
     return axes
 
-
 # Function to search for a university and visualize data
 def search_university(university_name):
     result = college_data.loc[college_data['UniversityName'] == university_name]
+    
     if len(result) == 0:
-        return "University not found!"
+        return "University not found in college_data!"
     
-    # Create some example plots (replace this with your actual visualizations)
-    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
-    axs = generate_rent_plot(rent_data, university_name, axs)
+    fig, axs = plt.subplots(2, 2, figsize=(15, 10))
     
+    # Rent Data
+    rent_row = rent_data.loc[rent_data['university'] == university_name]
+    if len(rent_row) == 1:
+        rent_row = rent_row.squeeze()
+        rent_data_values = [rent_row[col] for col in ['fmr_0', 'fmr_1', 'fmr_2', 'fmr_3', 'fmr_4']]
+        axs[0, 0].bar(["studio", "1 bedroom", "2 bedroom", "3 bedroom", "4 bedroom"], rent_data_values)
+        axs[0, 0].set_title('Rent Data')
+    else:
+        axs[0, 0].text(0.5, 0.5, 'Rent Data Not Available', ha='center', va='center')
+
     
-    """
-    axs[0, 1].bar(cost_of_living_data['Location'], cost_of_living_data['Cost'])
-    axs[0, 1].set_title('Cost of Living')
+    # Crime Data
+    # Assuming the university's state is available in college_data under a column 'State'
+    state = result.squeeze().get('STATE', None)
+    if state:
+        crime_row = crime_data.loc[crime_data['STATE'] == state]
+        if len(crime_row) == 1:
+            crime_row = crime_row.squeeze()
+            years = [str(year) for year in range(2011, 2021)]
+            crime_rates = [crime_row[year] for year in years]
+            axs[1, 1].plot(years, crime_rates, marker='o')
+            axs[1, 1].set_title('Violent Crime Rates per 100,000 inhabitants (2011–2020)')
+            axs[1, 1].set_xlabel('Year')
+            axs[1, 1].set_ylabel('Crime Rate')
+        else:
+            axs[1, 1].text(0.5, 0.5, 'Crime Data Not Available', ha='center', va='center')
+    else:
+        axs[1, 1].text(0.5, 0.5, 'State Information Not Available', ha='center', va='center')
     
-    axs[0, 2].bar(transportation_data['Location'], transportation_data['Transport_Cost'])
-    axs[0, 2].set_title('Transportation Costs')
-    
-    axs[1, 0].bar(crime_data['Location'], crime_data['Crime_Rate'])
-    axs[1, 0].set_title('Crime Data')
-    
-    axs[1, 1].bar(tuition_data['University'], tuition_data['Tuition'])
-    axs[1, 1].set_title('Tuition Data')
-    
-    axs[1, 2].axis('off')  # Hide the last plot
-    
-    """
     plt.tight_layout()
     return fig
+
 
 # Gradio interface
 iface = gr.Interface(
